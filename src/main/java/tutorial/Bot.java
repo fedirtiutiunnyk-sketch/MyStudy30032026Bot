068c26b9dcb3f4d07116ffddeb1887d3cf02fc75
fedirtiutiunnyk-sketch <fedir.tiutiunnyk@gmail.com>
2026-04-08 12:39:30 +0300

    Initial commit (not working)

---
+++ src/main/java/tutorial/Bot.java
@@ -0,0 +1,38 @@
+package tutorial;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return "MyStudy30032026Bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "my token";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var msg = update.getMessage();
+        var user = msg.getFrom();
+
+        System.out.println(user.getFirstName() + " wrote " + msg.getText());
+    }
+
+    public void sendText(Long who, String what) {
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString())
+                .text(what).build();
+        try {
+            execute(sm);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
